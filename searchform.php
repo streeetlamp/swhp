3112de7e1f08ad52b34b8211edbe4da96015e742
cody whitby <hi@codywhitby.net>
2017-10-09 14:49:46 -0400

    Built from commit 2ba3908 on branch master

--- searchform.php
+++ searchform.php
@@ -11,7 +11,7 @@
 <?php $unique_id = esc_attr( uniqid( 'search-form-' ) ); ?>
 
 <form role="search" method="get" class="search" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <label for="<?php echo $unique_id; ?>" class="visually-hidden">
+    <label for="<?php echo $unique_id; ?>" class="visually-hidden ir">
         <span class="screen-reader-text"><?php echo _x( 'Search for:', 'label', 'bonestheme' ); ?></span>
     </label>
     <input type="search" id="<?php echo $unique_id; ?>" class="search-field" placeholder="<?php echo esc_attr_x( 'Search &hellip;', 'placeholder', 'bonestheme' ); ?>" value="<?php echo get_search_query(); ?>" name="s" />
