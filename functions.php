decb9353d5de0feeaa5a80314cd58fc684ffd923
Cody W <cody@codywhitby.net>
2018-08-17 14:45:17 -0400

    Built from commit 7268b7c on branch master

--- functions.php
+++ functions.php
@@ -1,104 +1,87 @@
 <?php
 /**
  * Author: VCU Libraries Digital Engagement
- * URL: https://library.vcu.edu
- *
- * @package VCU_Libraries_WP
+ * URL: https://library.vcu.edu.
  */
 
 // LOAD BONES CORE (if you remove this, the theme will break)
-require_once( 'library/bones.php' );
+require_once 'library/bones.php';
 
 // Google Analytics
-require_once( 'library/inc/google-analytics.php' );
+require_once 'library/inc/google-analytics.php';
 
 // Disables trackbacks/pingbacks
-require_once( 'library/inc/disable-trackbacks.php' );
+require_once 'library/inc/disable-trackbacks.php';
 
 /**
- * LAUNCH BONES
+ * LAUNCH BONES.
  */
-function bones_ahoy() {
-  @ini_set( 'upload_max_size' , '64M' );
-  /** Allow editor style.
-  add_editor_style( get_stylesheet_directory_uri() . '/library/css/editor-style.css' ); */
+function bones_ahoy()
+{
+    @ini_set('upload_max_size', '64M');
+    @ini_set('display_errors', '0');
+    /* Allow editor style.
+    * add_editor_style( get_stylesheet_directory_uri() . '/library/css/editor-style.css' );
+    */
 
-  // launching operation cleanup
-  add_action( 'init', 'bones_head_cleanup' );
-  // A better title
-  add_filter( 'wp_title', 'rw_title', 10, 3 );
-  // remove WP version from RSS
-  add_filter( 'the_generator', 'bones_rss_version' );
-  // clean up gallery output in wp
-  add_filter( 'gallery_style', 'bones_gallery_style' );
-  // enqueue base scripts and styles
-  add_action( 'wp_enqueue_scripts', 'bones_scripts_and_styles', 999 );
+    // launching operation cleanup
+    add_action('init', 'bones_head_cleanup');
+    // A better title
+    add_filter('wp_title', 'rw_title', 10, 3);
+    // remove WP version from RSS
+    add_filter('the_generator', 'bones_rss_version');
+    // clean up gallery output in wp
+    add_filter('gallery_style', 'bones_gallery_style');
+    // enqueue base scripts and styles
+    add_action('wp_enqueue_scripts', 'bones_scripts_and_styles', 999);
 
-  // let's get language support going
-  load_theme_textdomain( 'bonestheme', get_template_directory() . '/library/translation' );
+    // let's get language support going
+    load_theme_textdomain('bonestheme', get_template_directory().'/library/translation');
 
-  // launching this stuff after theme setup
-  bones_theme_support();
+    // launching this stuff after theme setup
+    bones_theme_support();
 
-  // cleaning up random code around images
-  add_filter( 'the_content', 'bones_filter_ptags_on_images' );
-  // cleaning up excerpt
-  add_filter( 'excerpt_more', 'bones_excerpt_more' );
-
+    // cleaning up random code around images
+    add_filter('the_content', 'bones_filter_ptags_on_images');
+    // cleaning up excerpt
+    add_filter('excerpt_more', 'bones_excerpt_more');
 } /* end bones ahoy */
 
 // let's get this party started
-add_action( 'after_setup_theme', 'bones_ahoy' );
-
+add_action('after_setup_theme', 'bones_ahoy');
 
 /************* OEMBED SIZE OPTIONS *************/
 
-if ( ! isset( $content_width ) ) {
-	$content_width = 1240;
+if (!isset($content_width)) {
+    $content_width = 1240;
 }
-
-/************* THUMBNAIL SIZE OPTIONS *************/
-
-/** Thumbnail sizes
-// add_image_size( 'bones-thumb-600', 600, 150, true );
-// add_image_size( 'bones-thumb-300', 300, 100, true );
-
-// add_filter( 'image_size_names_choose', 'bones_custom_image_sizes' );
-
-// function bones_custom_image_sizes( $sizes ) {
-//     return array_merge( $sizes, array(
-//         'bones-thumb-600' => __('600px by 150px'),
-//         'bones-thumb-300' => __('300px by 100px'),
-//     ) );
-// }
- */
-
 
 /**
- * THEME CUSTOMIZE
+ * THEME CUSTOMIZE.
  */
-function bones_theme_customizer( $wp_customize ) {
-  $wp_customize->remove_section( 'colors' );
-  $wp_customize->remove_section( 'background_image' );
+function bones_theme_customizer($wp_customize)
+{
+    $wp_customize->remove_section('colors');
+    $wp_customize->remove_section('background_image');
 }
 
-add_action( 'customize_register', 'bones_theme_customizer' );
-
+add_action('customize_register', 'bones_theme_customizer');
 
 /**
  * Testing our environment.
  *
  * @returns true if on production server false if not
  */
-function we_are_live() {
-  $host = home_url();
-  
-  if ( preg_match( '/\.test/', $host ) ) {
-		return false;
-  }
-  return true;
-}
+function we_are_live()
+{
+    $host = home_url();
 
+    if (preg_match('/\.test/', $host)) {
+        return false;
+    }
+
+    return true;
+}
 
 // Enable support for HTML5 markup.
 add_theme_support(
@@ -111,18 +94,18 @@ add_theme_support(
     )
 );
 
-
-  if ( ! function_exists( 'the_field' ) ) {
-		add_action( 'admin_notices', 'bones_acf_notice' );
+  if (!function_exists('the_field')) {
+      add_action('admin_notices', 'bones_acf_notice');
   }
 
 /**
- * ACF Bones Notice
+ * ACF Bones Notice.
  */
-function bones_acf_notice() {
-	?>
+function bones_acf_notice()
+{
+    ?>
 	<div class="update-nag notice" style="display:block; margin:20px 0;">
-	<h3><?php echo( 'Install Advanced Custom Fields Pro please or nothing will work probably!' ); ?></h3>
+	<h3><?php echo  'Install Advanced Custom Fields Pro please or nothing will work probably!'; ?></h3>
 	</div>
 	<?php
 }
@@ -130,41 +113,42 @@ function bones_acf_notice() {
 /*
 * ACF options page
 */
-if ( function_exists( 'acf_add_options_page' ) ) {
-
-		  acf_add_options_page(
+if (function_exists('acf_add_options_page')) {
+    acf_add_options_page(
           array(
-			  'page_title'  => 'Site Options',
-			  'menu_title'  => 'Site Options',
-			  'menu_slug'   => 'options',
-			  'capability'  => 'edit_posts',
-			  'redirect'    => false,
-		  )
-			  );
-
+              'page_title' => 'Site Options',
+              'menu_title' => 'Site Options',
+              'menu_slug' => 'options',
+              'capability' => 'edit_posts',
+              'redirect' => false,
+          )
+              );
 }
 
 //since WP wont just give a alt tag
-function get_the_post_thumbnail_alt( $post_id ) {
-  return get_post_meta( get_post_thumbnail_id( $post_id ), '_wp_attachment_image_alt', true );
+function get_the_post_thumbnail_alt($post_id)
+{
+    return get_post_meta(get_post_thumbnail_id($post_id), '_wp_attachment_image_alt', true);
 }
 
-function the_post_thumbnail_alt( $post_id ) {
-  echo get_the_post_thumbnail_alt( $post_id) ;
+function the_post_thumbnail_alt($post_id)
+{
+    echo get_the_post_thumbnail_alt($post_id);
 }
-
 
 /*
 SiteImprove would much prefer strong over b so we will do that.
 */
-function change_b_to_strong( $content ) {
-  str_replace( '<b>', '<strong>', $content );
-  str_replace( '</b>', '</strong>', $content );
-  return $content;
+function change_b_to_strong($content)
+{
+    str_replace('<b>', '<strong>', $content);
+    str_replace('</b>', '</strong>', $content);
+
+    return $content;
 }
-add_filter( 'the_content', 'change_b_to_strong' );
+add_filter('the_content', 'change_b_to_strong');
 
 // Hide ACF from admin menu if live
-if ( we_are_live() ) {
-    add_filter( 'acf/settings/show_admin', '__return_false' );
+if (we_are_live()) {
+    add_filter('acf/settings/show_admin', '__return_false');
 }
--- index.php
+++ index.php
@@ -29,26 +29,27 @@ get_template_part( 'library/templates/the-header' );
                 <header class="article-header entry-header">
                     <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
                 </header>
-            <?php
+
+                <?php
                 the_excerpt();
                 echo '<a class="excerpt-read-more small" href="' . get_permalink() . '" title="' . __( 'Read ', 'bonestheme' ) . esc_attr( get_the_title() ) . '">' . __( 'Continue Reading &raquo;', 'bonestheme' ) . '</a>';
-            else :
-    			the_content();
-            endif;
+                else :
+                    the_content();
+                endif;
                 ?>
 
 			<?php
 			// check if the flexible content field has rows of data
 			if ( have_rows( 'flexible_content' ) ) :
 				while ( have_rows( 'flexible_content' ) ) :
-                        the_row();
+                    the_row();
 					get_template_part( 'library/flex-content-loop' );
-					endwhile;
-				endif;
+                endwhile;
+            endif;
 			?>
 			</section>
 
-		</article> 
+		</article>
 
 	<?php endwhile; ?>
 
