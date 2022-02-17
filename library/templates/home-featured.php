915faf4ada95729db10febec16b6cf965c38a9ae
cody <hi@codywhitby.net>
2022-02-17 15:18:24 -0500

    Built from commit ec40e62 on branch master

--- library/templates/home-featured.php
+++ library/templates/home-featured.php
@@ -9,20 +9,26 @@
 ?>
 
 <header class="featured-header">
-    <h3>Featured</h3>
+	<h3>Featured</h3>
 </header>
 
 <section class="featured-items" aria-label="featured items">
-    <?php
-    $feat_posts = get_field( 'post_feature', 'options' );
+	<?php
+	$feat_posts = get_field( 'post_feature', 'options' );
 
-    if ( $feat_posts ) :
-        foreach ( $feat_posts as $feat_post ) :
-        ?>
-        <div class="featured-item">
-            <a href="<?php echo get_permalink( $feat_post ); ?>"><img alt="<?php the_post_thumbnail_alt( $feat_post ); ?>" src="<?php echo get_the_post_thumbnail_url( $feat_post, 'medium' ); ?>"><?php echo $feat_post->post_title; ?></a>
-        </div>
-        <?php endforeach; ?>
+	if ( $feat_posts ) :
+		foreach ( $feat_posts as $feat_post ) :
+			$feat_post_title = get_field( 'featured_post_title', $feat_post );
+			if  ( $feat_post_title ) :
+				$feat_title = $feat_post_title;
+			else :
+				$feat_title = $feat_post->post_title;
+			endif;
+			?>
+			<div class="featured-item">
+				<a href="<?php echo get_permalink( $feat_post ); ?>"><img alt="<?php the_post_thumbnail_alt( $feat_post ); ?>" src="<?php echo get_the_post_thumbnail_url( $feat_post, 'medium' ); ?>"><?php echo $feat_title; ?></a>
+			</div>
+		<?php endforeach; ?>
 
-    <?php endif; ?>
+	<?php endif; ?>
 </section>
